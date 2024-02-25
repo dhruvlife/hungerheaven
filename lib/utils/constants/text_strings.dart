7fdd5742f1a712eb5d346321f5fc499a0a548caf
syq <dhruv.life3110@gmail.com>
2024-02-25 20:52:40 +0530

    9:00PM 25-4-24

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -12,13 +12,13 @@ class TTexts {
 
 
   // -- OnBoarding Texts
-  static const String onBoardingTitle1 = "Choose your product";
-  static const String onBoardingTitle2 = "Select Payment Method";
-  static const String onBoardingTitle3 = "Deliver at your door step";
+  static const String onBoardingTitle1 = "Hunger Heaven: Sustaining Smiles, Sharing Meals";
+  static const String onBoardingTitle3 = "Feast Fleet: Hunger Heaven";
+  static const String onBoardingTitle2 = "Harvest Heroes: Hunger Heaven";
 
-  static const String onBoardingSubTitle1 = "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!";
-  static const String onBoardingSubTitle2 = "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!";
-  static const String onBoardingSubTitle3 = "From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!";
+  static const String onBoardingSubTitle1 = "Transforming Hunger into Hope!";
+  static const String onBoardingSubTitle3 = "Delivering Sustenance, Spreading Joy!";
+  static const String onBoardingSubTitle2 = "From Fields to Doorsteps, Nourishing Communities!";
 
 
   // -- Authentication Forms
